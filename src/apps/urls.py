97861225f9b5f1a32c24b63b1ee96f593202826d
Abhijith Anilkumar <abhijithanilkumar@live.com>
2017-06-15 23:54:17 +0530

    All Apps Page

--- src/apps/urls.py
+++ src/apps/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.appPage, name="apps"),
     url(r'^(?P<num>[0-9]+)/$', views.appPage, name="appPage"),
+    url(r'^top/$', views.topPage, name="topPage"),
+    url(r'^new/$', views.newPage, name="newPage"),
 ]
--- src/apps/views.py
+++ src/apps/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, get_object_or_404
-from models import App, Release
+from models import App, Release, Tag
 from markdownx.utils import markdownify
 
 # Create your views here.
@@ -22,4 +22,15 @@ def appPage(request, num=0):
         return render(request, 'page.html', {'app':app, 'releases':releases, 'authors':authors, 'latest':latest})
     else:
         apps = App.objects.all()
-        return render(request, 'page.html', {'apps':apps})
+        tags = Tag.objects.all()
+        return render(request, 'apps.html', {'apps':apps, 'tags':tags})
+
+def topPage(request):
+    apps = App.objects.all().order_by('-downloads')
+    tags = Tag.objects.all()
+    return render(request, 'apps.html', {'apps':apps, 'tags':tags})
+
+def newPage(request):
+    apps = App.objects.all().order_by('-latest_release_date')
+    tags = Tag.objects.all()
+    return render(request, 'apps.html', {'apps':apps, 'tags':tags})
