d6dfb806da730385bf03145dc8e55cc79a854cc0
Abhijith Anilkumar <abhijithanilkumar@live.com>
2017-06-20 20:43:16 +0530

    Maintainer and Developer Workflows!

--- src/backend/forms.py
+++ src/backend/forms.py
@@ -23,3 +23,50 @@ class CreateAppForm(forms.ModelForm):
             'authors',
             'editors',
         ]
+
+class EditAppForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(forms.ModelForm, self).__init__(*args, **kwargs)
+        self.fields['title'].label = "Title *"
+        self.fields['abstract'].label = "Abstract (255 characters) *"
+        self.fields['description'].label = "Description (Markdown Supported!) *"
+        self.fields['authors'].label = "Authors of the App"
+        self.fields['tags'].label = "Tags related to the App"
+        self.fields['website'].label = "Website of the App"
+        self.fields['tutorial'].label = "Tutorial Website Link"
+        self.fields['coderepo'].label = "Link to the Code Repository"
+        self.fields['contact'].label = "Contact Email"
+
+    def clean(self):
+        return self.cleaned_data
+
+    class Meta:
+        model = apps.get_model('apps', 'App')
+        exclude = [
+            'editors',
+            'stars',
+            'votes',
+            'downloads',
+        ]
+
+class ReleaseForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(forms.ModelForm, self).__init__(*args, **kwargs)
+        self.fields['version'].label = "Version *"
+        self.fields['require'].label = "Requires ns-3 Version *"
+        self.fields['notes'].label = "Release Notes *"
+        self.fields['dependencies'].label = "Package Dependencies *"
+        self.fields['filename'].label = "Release File (bakeconf.xml)"
+        self.fields['url'].label = "Release Url"
+
+    def clean(self):
+        return self.cleaned_data
+
+    class Meta:
+        model = apps.get_model('apps', 'Release')
+        exclude = [
+            'app',
+            'date',
+        ]
--- src/backend/views.py
+++ src/backend/views.py
@@ -3,22 +3,85 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from forms import CreateAppForm
+from forms import CreateAppForm, EditAppForm, ReleaseForm
+from django.apps import apps
 
 @login_required
 def createApp(request):
-    if request.method == 'GET':
-        form = CreateAppForm()
-    return render(request, 'create.html',{'form':form})
+    if request.user.is_superuser:
+        if request.method == 'GET':
+            form = CreateAppForm()
+        elif request.method == 'POST':
+            form = CreateAppForm(request.POST)
+            if form.is_valid():
+                new_app = form.save()
+                new_app.save()
+                return render(request, 'message.html', {'message': "New App Page created Successfully!"})
+    else:
+        return render(request, 'message.html', {'message': "You are not authorized to view this page!"})
+    return render(request, 'create.html', {'form':form})
 
 @login_required
 def editApp(request, num):
-    return render(request, 'edit.html')
+    App = apps.get_model('apps', 'App')
+    try:
+        edit_app = App.objects.get(id=num)
+    except:
+        return render(request, 'message.html', {'message': "Requested App does not Exist!"})
+    editors = edit_app.editors.all()
+    if request.user in editors or request.user.is_staff:
+        if request.method == 'GET':
+            form = EditAppForm(instance=edit_app)
+        elif request.method == 'POST':
+            form = EditAppForm(request.POST, instance=edit_app)
+            if form.is_valid():
+                edited_app = form.save()
+                edited_app.save()
+                return render(request, 'message.html', {'message': "App Page edited Successfully!"})
+    else:
+        return render(request, 'message.html', {'message': "You are not authorized to view this page!"})
+    return render(request, 'edit.html', {'form':form})
 
 @login_required
 def createRelease(request, num):
-    return render(request, 'create_release.html')
+    App = apps.get_model('apps', 'App')
+    try:
+        release_app = App.objects.get(id=num)
+    except:
+        return render(request, 'message.html', {'message': "Requested App does not Exist!"})
+    editors = release_app.editors.all()
+    if request.user in editors or request.user.is_staff:
+        if request.method == 'GET':
+            form = ReleaseForm()
+        elif request.method == 'POST':
+            form = ReleaseForm(request.POST)
+            if form.is_valid():
+                new_release = form.save(commit=False)
+                new_release.app = release_app
+                new_release.save()
+                return render(request, 'message.html', {'message': "Release added Successfully!"})
+    else:
+        return render(request, 'message.html', {'message': "You are not authorized to view this page!"})
+    return render(request, 'create_release.html', {'form':form})
 
 @login_required
 def editRelease(request, num):
-    return render(request, 'edit_release.html')
+    Release = apps.get_model('apps', 'Release')
+    App = apps.get_model('apps', 'App')
+    try:
+        edit_release = Release.objects.get(id=num)
+    except:
+        return render(request, 'message.html', {'message': "Requested App does not Exist!"})
+    editors = edit_release.app.editors.all()
+    if request.user in editors or request.user.is_staff:
+        if request.method == 'GET':
+            form = ReleaseForm(instance=edit_release)
+        elif request.method == 'POST':
+            form = ReleaseForm(request.POST, instance=edit_release)
+            if form.is_valid():
+                edited_release = form.save()
+                edited_release.save()
+                return render(request, 'message.html', {'message': "Release edited Successfully!"})
+    else:
+        return render(request, 'message.html', {'message': "You are not authorized to view this page!"})
+    return render(request, 'edit_release.html', {'form':form})
