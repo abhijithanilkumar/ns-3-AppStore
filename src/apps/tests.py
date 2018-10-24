e97317a372cc0aec3335f25a8f7babc01e7609a7
Abhijith Anilkumar <abhijithanilkumar@live.com>
2018-10-24 23:38:41 +0530

    Baseline code for tests

--- src/apps/tests.py
+++ src/apps/tests.py
@@ -2,5 +2,16 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from models import App
 
-# Create your tests here.
+class AppTestCase(TestCase):
+    def setUp(self):
+        App.objects.create(name='App1',
+                           title='App Title',
+                           app_type='F',
+                           abstract='This is a test App',
+                           description='This is a test App')
+    def test_app_created(self):
+        app = App.objects.get(name='App1')
+        self.assertTrue(isinstance(app, App))
+        self.assertEqual(app.title, 'App Title')
