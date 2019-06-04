0c5466421030b3d1e98c858f3c3b4e9cfd52bd2b
Mishal Shah <shahmishal1998@gmail.com>
2019-06-04 12:57:37 +0530

    install api core logic

--- src/backend/views_api.py
+++ src/backend/views_api.py
@@ -9,5 +9,31 @@ from django.http import JsonResponse
 
 @api_view(['GET'])
 def install(request, module_name, version=None):
-	response = {}
-	return Response(response)
+	try:
+        app = App.objects.get(name=module_name)
+    except BaseException:
+        # Request 404 for App not found on the AppStore
+        response['status'] = 404
+        response['message'] = "Module: " + module_name + " was not found on the ns-3 AppStore."
+        return Response(response)
+    # if version is not specified in the API Call, send the latest file config, 
+    # else the specified version config
+    if version==None:
+        app_release = Release.objects.filter(app=app).order_by('-version').first()
+    else:
+        app_release = Release.objects.filter(app=app, version=version).first()
+        # Send 404 for App with not the requested version
+        if app_release==None:
+            response['status'] = 404
+            response['message'] = "Module: " + module_name + " with version: " + version + " was not found on the ns-3 AppStore."
+            return Response(response)
+
+    response['name'] = app.name
+    response['app_type'] = app.app_type
+    response['coderepo'] = app.coderepo
+    response['version'] = app_release.version
+    response['bakefile_url'] = settings.MEDIA_URL + str(app_release.filename)
+    response['status'] = 200
+    response['message'] = "Module: " + module_name + " with version: " + app_release.version + " found on the ns-3 AppStore."
+    response = json.loads(json.dumps(response))
+    return Response(response)
