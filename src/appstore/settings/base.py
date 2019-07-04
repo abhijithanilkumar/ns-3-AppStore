1a637f01babe33de828c7de791d7a2a0921b4921
Mishal Shah <shahmishal1998@gmail.com>
2019-07-04 21:28:25 +0530

    changed the default page size to 20

--- src/appstore/settings/base.py
+++ src/appstore/settings/base.py
@@ -122,7 +122,7 @@ REST_FRAMEWORK = {
         'user': '1000/day'
     },
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 2
+    'PAGE_SIZE': 20
 }
 
 ROOT_URLCONF = 'appstore.urls'
