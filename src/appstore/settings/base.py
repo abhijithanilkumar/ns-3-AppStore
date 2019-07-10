388640fc7908ae616077a5cc1f6cdcb93482f6a7
Mishal Shah <shahmishal1998@gmail.com>
2019-07-10 18:19:10 +0530

    added gitlab oauth

--- src/appstore/settings/base.py
+++ src/appstore/settings/base.py
@@ -49,7 +49,7 @@ AUTHENTICATION_BACKENDS = (
     'social_core.backends.google.GoogleOpenId',  # for Google authentication
     'social_core.backends.google.GoogleOAuth2',  # for Google authentication
     'social_core.backends.github.GithubOAuth2',  # for Github authentication
-    'social_core.backends.gitlab.GitLabOAuth2',  # for Github authentication
+    'social_core.backends.gitlab.GitLabOAuth2',  # for GitLab authentication
     'social_core.backends.facebook.FacebookOAuth2',  # for Facebook authentication
     'django.contrib.auth.backends.ModelBackend'
 )
