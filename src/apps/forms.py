6a325d62a8c75c2282403fdc1c92fb14566b3f82
Mishal Shah <shahmishal1998@gmail.com>
2019-07-11 11:18:17 +0530

    Feature: Search filter for Categories & App Type added (#49)
    * migrated from python2.7 to python3.6, url convention changed from url to path, ported to Django2.0, updated the packages
    
    updated versions for packages in base.txt
    
    fix releases form to support file upload
    
    removed all commented lines after testing
    
    REST API started for install
    
    install api core logic
    
    modified install API to take care of empty bakefile
    
    modified the search API to use ?
    
    added restframework in requirements:base.txt
    
    made changes to font-weight
    
    Added User Guide (#45)
    
    * user guide
    
    * added more information from the app-store-overview
    
    * added numbers, and details markdown to improve readibility
    
    Port the ns-3 AppStore to Python3 and Django 2.0 (#47)
    
    * migrated from python2.7 to python3.6, url convention changed from url to path, ported to Django2.0, updated the packages
    
    * code formatted according to PEP 8 style guide
    
    * updated versions for packages in base.txt
    
    * fix releases form to support file upload
    
    * removed all commented lines after testing
    
    migrated from python2.7 to python3.6, url convention changed from url to path, ported to Django2.0, updated the packages
    
    code formatted according to PEP 8 style guide
    
    removed all commented lines after testing
    
    removed extra lines
    
    modified the install API to use serializers
    
    changed code to follow PEP8 guidelines
    
    Search API changed to use viewsets, WIP: version parameter in response
    
    viewset for search API implemented along with the version
    
    updated API Documentation
    
    view all forks, utilities added
    
    added forks and utlities link in nav panel
    
    search API modified to send all the apps info for list in Bake
    
    added search filters for categories and app type, removed print statements
    
    pagination started
    
    merge conflicts resolved
    
    * added forks and utlities link in nav panel
    
    * added search filters for categories and app type, removed print statements
    
    * removed same import code
    
    * code modified as per PEP8 huideline
    
    * removed extra space
    
    * fixed extra line
    
    * fixed extra lines
    
    * commented out the search filter code
    
    * filters changed to follow JS and alongwith the sort by option
    
    * added space after the filter by word
    
    * removed the extra padding in the app_button, full class
    
    * alignments fixed, comments added
    
    * removed the crispy form tag
    
    * added message in case of no results are found

--- src/apps/forms.py
+++ src/apps/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Comment
+from .models import Comment, Tag, App
 
 
 class CommentForm(forms.ModelForm):
@@ -19,3 +19,20 @@ class CommentForm(forms.ModelForm):
             'title',
             'content'
         ]
+
+
+# This form can be used to take inputs from the User and filter the search results. 
+# The code is not in use right now, as we do not accept User inputs for filtering. 
+# The form can be enhanced to accept more fields and filter based on it in the future, if required.
+class SearchFilterForm(forms.Form):
+    tag = forms.ModelChoiceField(
+        label="Select the Tag",
+        required=False,
+        queryset=Tag.objects.all())
+    app_type = forms.ChoiceField(
+        label="Select the type of App",
+        required=False,
+        choices=App.TYPES)
+
+    def clean(self):
+        return self.cleaned_data
