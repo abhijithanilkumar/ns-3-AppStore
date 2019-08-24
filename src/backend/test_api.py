beafc1dee6213e8c4a5f112cb51d2cf09984a50d
Mishal Shah <shahmishal1998@gmail.com>
2019-08-25 00:10:01 +0530

    documented the tests

--- src/backend/test_api.py
+++ src/backend/test_api.py
@@ -4,14 +4,27 @@ from django.urls import reverse
 
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
-from apps.models import App
+from apps.models import App, NsRelease, Release
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 
 class AppSearchAPIViewTestCase(APITestCase):
 	url = reverse("backend:search")
 
-	def test_search(self):
+	def setUp(self):
+		DApp = App.objects.create(name='ns-3-gym',
+							title='Gym',
+							app_type='F',
+							abstract='This is a test App for Development',
+							description='This is a test App for Development')
 
+		ns=NsRelease.objects.create(name='ns-3.29',url='https://www.nsnam.org/')
+		Release.objects.create(app=DApp,version='TestVersion',require=ns,date= '2018-12-27',notes= 'TestNote',filename = SimpleUploadedFile('filename.txt',''),url='https://www.nsnam.org/')
+
+	def test_search(self):
+		"""
+		Test to verify the search API Functionality
+		"""
 		# exisiting application
 		response = self.client.post(self.url, {'q':'gym', 'ns':'ns-3.29'})
 		self.assertEqual(200, response.status_code)
@@ -19,6 +32,7 @@ class AppSearchAPIViewTestCase(APITestCase):
 		# when no params are passed, returns all the apps
 		response = self.client.post(self.url)
 		self.assertEqual(200, response.status_code)
+		self.assertTrue(len(json.loads(response.content)) > 0)
 
 		# random keyword, no such app found
 		response = self.client.post(self.url, {'q':'abcdefghi', 'ns': 'ns-3.21'})
@@ -32,13 +46,34 @@ class AppSearchAPIViewTestCase(APITestCase):
 class AppInstallAPIViewTestCase(APITestCase):
 	url = reverse("backend:install")
 
-	def test_install(self):
-		
-		response = self.client.post(self.url, {'module_name':'ns3-gym', 'ns':'ns-3.29'})
-		self.assertEqual(404, response.status_code)
+	def setUp(self):
+		DApp = App.objects.create(name='ns3-gym',
+							title='Gym',
+							app_type='F',
+							abstract='This is a test App for Development',
+							description='This is a test App for Development')
 
+		ns=NsRelease.objects.create(name='3.29',url='https://www.nsnam.org/')
+		Release.objects.create(app=DApp,version='TestVersion',require=ns,date= '2018-12-27',notes= 'TestNote',filename = SimpleUploadedFile('filename.txt',''),url='https://www.nsnam.org/')
+
+
+	def test_install(self):
+		"""
+		Test to verify the install API Functionality
+		"""
+
+		# when the app exists
+		response = self.client.post(self.url, {'module_name':'ns3-gym', 'ns':'ns-3.29'})
+		self.assertEqual(200, response.status_code)
+
+		# app exists, with multiple ns versions
+		response = self.client.post(self.url, {'module_name':'ns3-gym', 'ns':'[ns-3.29, ns-3.27]'})
+		self.assertEqual(200, response.status_code)
+
+		# random keyword, no such app found
 		response = self.client.post(self.url, {'module_name':'abcdeghi', 'ns':'[ns-3.29, ns-3.27]'})
 		self.assertEqual(404, response.status_code)
 
+		# random with multiple ns versions, no such app found
 		response = self.client.post(self.url, {'module_name':'abcdeghi', 'version': '1.10', 'ns':'[ns-3.29, ns-3.27]'})
 		self.assertEqual(404, response.status_code)
